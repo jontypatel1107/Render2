d71e6d8af76ec80c624867d9483e2342eac56c1f
jontypatel1107 <jontypatel1107@gmail.com>
2026-02-04 11:57:30 +0530

    Render deployment

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+
+const app = express();
+
+const users = [
+  { id: 1, name: "Arjun", role: "student" },
+  { id: 2, name: "Priyesha", role: "mentor" }
+];
+
+app.get("/", (req, res) => {
+  res.send("Express server is running");
+});
+
+app.get("/users", (req, res) => {
+  res.status(200).json(users);
+});
+
+app.get("/user/:test", (req, res) => {
+    console.log(req.params);
+    console.log("First Code");
+    res.status(200).json(users);
+});
+
+app.get("/users/three", (req, res) => {
+    console.log(req.params);
+    console.log("Second Code");
+    res.status(200).json(users);
+});
+
+app.get("/users/:id", (req, res) => {
+  const userId = Number(req.params.id);
+  const user = users.find(u => u.id === userId);
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  res.status(200).json(user);
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
